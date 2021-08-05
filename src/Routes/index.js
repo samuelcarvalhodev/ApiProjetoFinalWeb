176fa8d91b8a0fc864e6e3de4c8aa1aea6d3aa5c
samuelcarvalhodev <samuelmacal.2015@gmail.com>
2021-08-04 22:02:42 -0300

    controle de rota por controller

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,15 +1,23 @@
 import { Router } from 'express';
 import {Post} from '../Controllers/Global/UserController';
-import {Login, LoginPool} from '../Controllers/Global/AuthController';
+import {Login} from '../Controllers/Global/AuthController';
+import Option from './Public/option.routes';
+import Evaluation from './Public/evaluation.routes';
+import Menu from './Admin/menu.routes';
 
 
 const routes = Router();
+routes.use("/option", Option);
+routes.use("/evaluation", Evaluation);
+routes.use("/menu", Menu);
 
 routes.get("/", (get)=>{
     console.log("Conexão bem sucedida!");
 })
 
 routes.post("/", Post);
+// routes.get("/", Get);
 routes.post("/login", Login);
+
 
 export default routes;
