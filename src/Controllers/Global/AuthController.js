1cc5c2700c3780e587c2c1e59e04848d2f0350b2
samuelcarvalhodev <samuelmacal.2015@gmail.com>
2021-08-04 23:18:53 -0300

    refatoração login

--- src/Controllers/Global/AuthController.js
+++ src/Controllers/Global/AuthController.js
@@ -1,37 +1,35 @@
 import client from "../../../db_config";
-const pg = require('pg')
+const pg = require("pg");
 const pool = new pg.Pool();
 
-
-
 export async function Login(req, res) {
-  const { username, password, cro } = req.body;
+  const { registration, password, cro } = req.body;
   let User = null;
   await client.connect();
-  //Verificando existencia
-  const userNameValidate = "SELECT username FROM usuario WHERE username = $1";
-  const userNameResponse = await client.query(userNameValidate, [username]);
-  if (
-    userNameResponse.rowCount < 1 ||
-    userNameResponse == undefined ||
-    userNameResponse == null ||
-    userNameResponse.rowCount == null
-  ) {
+
+  const registrationValidate =
+    "SELECT registration FROM usuario WHERE registration = $1";
+  const registrationResponse = await client.query(registrationValidate, [
+    registration,
+  ]);
+
+  const croValidate = "SELECT cro FROM usuario WHERE cro = $1";
+  const croResponse = await client.query(croValidate, [cro]);
+
+  if (registrationResponse.rowCount < 1 && croResponse.rowCount < 1) {
     await client.end();
-    res.json({ message: "Usuário não existe" });
+    return res.json({ message: "Usuário não existe" });
   } else {
     //Validando credencial
     const loginValidate =
-      "SELECT username, password, cro, registration, sex, course, typeuser FROM usuario WHERE username = $1 and (password = $2 or cro = $3);";
-    User = await client.query(loginValidate, [username, password, cro]);
+      "SELECT username, password, cro, registration, sex, course, typeuser FROM usuario WHERE password = $1 and (registration = $2 or cro = $3);";
+    User = await client.query(loginValidate, [password, registration, cro]);
     if (User.rowCount == 0 || User == undefined || User == null) {
-        await client.end();
-      res.json({ message: "Login incorreto" });
+      await client.end();
+      return res.json({ message: "Login incorreto" });
     } else {
-        await client.end();
-      res.json({ user: User.rows, message: "Login efetuado com sucesso!" });
+      await client.end();
+      return res.json({ user: User.rows, message: "Login efetuado com sucesso!" });
     }
   }
-
-  
 }
