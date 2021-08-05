5491339ff7146c18a666b788184de12cd1076bca
samuelcarvalhodev <samuelmacal.2015@gmail.com>
2021-08-04 23:19:27 -0300

    refatoração de tabela

--- src/Database/Migration/Create.js
+++ src/Database/Migration/Create.js
@@ -29,7 +29,7 @@ async function createTables() {
     "CREATE TYPE CourseUser AS ENUM('CC', 'SI', 'Eng.Minas', 'Eng.Civil', 'Eng.Ambiental')"
   );
   await client.query(
-    "CREATE TABLE usuario( id serial PRIMARY KEY UNIQUE NOT NULL, username VARCHAR(50) UNIQUE NOT NULL, password VARCHAR(50) NOT NULL, cro VARCHAR(50) NULL, registration VARCHAR(50) NULL, sex SexUser NULL, course CourseUser NULL, typeUser TypeUser NOT NULL)"
+    "CREATE TABLE usuario( id serial PRIMARY KEY UNIQUE NOT NULL, username VARCHAR(50) NOT NULL, password VARCHAR(50) NOT NULL, cro VARCHAR(50) NULL, registration VARCHAR(50) NULL, sex SexUser NULL, course CourseUser NULL, typeUser TypeUser NOT NULL)"
   );
 
   await client.query(
