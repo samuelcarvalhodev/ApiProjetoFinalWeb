3ef40a7a5301a6f402257ab05ca850fb51f0e24d
Herdeson Mourão <herdesondev@gmail.com>
2021-08-22 15:28:42 -0300

    Feat: :sparkles: implementando dotenv

--- ApiProjetoFinalWeb/db_config.js
+++ ApiProjetoFinalWeb/db_config.js
@@ -1,11 +1,11 @@
 import pg from "pg";
 
 const client = new pg.Pool({
-    user: "postgres",
-    password: "159753" ,
-    host: "localhost",
-    database: "ufcFood",
-    port: 5432,
+    user: process.env.DB_user,
+    password: process.env.DB_password,
+    host: process.env.DB_host,
+    database: process.env.DB_database,
+    port: process.env.DB_port,
 })
 
 export default client;
