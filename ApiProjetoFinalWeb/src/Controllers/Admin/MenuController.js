637a102c604eaeae6993cc4c207c40ab1427c556
Herdeson Mourão <herdesondev@gmail.com>
2021-08-22 20:01:57 -0300

    Feat: :sparkles: implementando método de edit e de delete no menu

--- ApiProjetoFinalWeb/src/Controllers/Admin/MenuController.js
+++ ApiProjetoFinalWeb/src/Controllers/Admin/MenuController.js
@@ -2,8 +2,7 @@ import express from "express";
 import client from "../../../db_config";
 
 export async function Post(req, res) {
-  const { type_option, option_name, date_menu, option_id, image_food } =
-    req.body;
+  const { type_option, option_name, date_menu, option_id, image_food } = req.body;
 
   const registerMenu =
     "INSERT INTO cardapio(type_option, option_name, date_menu, option_id, image_food)	VALUES ($1,$2,$3,$4,$5);";
@@ -53,5 +52,59 @@ export async function GetById(req, res) {
     return res.json({ Menu: menu.rows });
   } catch (error) {
     return res.json({ message: "Não existe nenhum correspondente" });
+  }
+}
+
+export async function Dell(req, res){
+  try {
+    const { id } = req.params;
+
+    const deleteMenu = "DELETE FROM cardapio WHERE id = $1;";
+  
+    const menu = await client.query(deleteMenu, [id]);
+  
+    if(menu.rowCount == 0){
+      return res.json({message: "Não existe nenhum menu correspondente"});
+    } else{
+      return res.json({message: "Deletado com sucesso"})
+    }
+  } catch (error) {
+    return res.json({message: "Não existe nenhum correspondente"});
+  }
+}
+
+export async function Edit(req, res){
+  try {
+    const { id } = req.params;
+    const { 
+      type_option,
+      option_name,
+      date_menu,
+      option_id,
+      image_food } = req.body;
+
+    const updateMenu = `UPDATE menu SET 
+    type_option = $1,
+    option_name = $2,
+    date_menu = $3,
+    option_id = $4,
+    image_food = $5 WHERE id = $6`;
+
+    const menu = await client.query(updateMenu, [
+      type_option,
+      option_name,
+      date_menu,
+      option_id,
+      image_food,
+      id
+    ])
+    
+    if(menu.rowCount == 0){
+      return res.json({message: "Não existe nenhum menu correspondente"});
+    } else{
+      return res.json({message: "menu atualizado com sucesso"})
+    }
+  } catch (error) {
+    return res.json({message: "Não existe nenhum correspondente"});
   }
 }
--- ApiProjetoFinalWeb/src/Routes/Admin/menu.routes.js
+++ ApiProjetoFinalWeb/src/Routes/Admin/menu.routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import {GetAll, GetById, Post} from '../../Controllers/Admin/MenuController';
+import {GetAll, GetById, Post, Dell, Edit} from '../../Controllers/Admin/MenuController';
 
 const routes = Router();
 
 routes.post("/", Post);
 routes.get("/", GetAll);
 routes.get("/:id", GetById);
+routes.delete("/:id", Dell);
+routes.put("/:id", Edit);
 
 export default routes;
